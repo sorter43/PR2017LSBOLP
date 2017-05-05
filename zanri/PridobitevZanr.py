3af2c9a86058e40cfa195298c1c630c4c51a12f8
blaz <bocepek027@gmail.com>
2017-05-05 21:23:58 +0200

    nekja opazanj
    dodal sem dve mape z dvema .txt datotekam ki na kratko opišeta moje
    opažanja zraven še python skripta s katerem sem pridobil zanre in orange
    okolje kjer sem prikazal grafično porazdelitev podatkov.

---
+++ zanri/PridobitevZanr.py
@@ -0,0 +1,25 @@
+from csv import *
+
+filmi = list()
+
+reader = DictReader(open("anime.csv","rt", encoding = "utf-8"))
+
+
+for row in reader:
+    filmi.append(row["genre"])
+
+vseZanre = {}
+for zanre in filmi:
+    zanr = zanre.split(",")
+    for z in zanr:
+        if z in vseZanre:
+            vseZanre[z] = vseZanre[z]+1
+        else:
+            vseZanre[z] = 1
+
+print("Zapisovanje v datoteko")
+f = open("statistikaZanrov.csv" ,"w")
+f.write("zanr,statistikaZanra\n")
+for k in vseZanre:
+    f.write(str(k)+","+str(vseZanre[k])+"\n")
+print("done!!!!!!!!!!!!!!!!!!!!!!!!!")
